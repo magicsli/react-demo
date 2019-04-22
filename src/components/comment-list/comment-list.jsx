5c9e9e884fcebd7b6346f956fc99d764fbf38b90
magicsli <xiaoming@qq.com>
2019-04-22 17:13:49 +0800

    debug

--- src/components/comment-list/comment-list.jsx
+++ src/components/comment-list/comment-list.jsx
@@ -1,6 +1,7 @@
+
 import React, { Component } from 'react';
 import PropTypes from "prop-types";
-
+import CommentItem  from "../comment-item/comment-item"
 import "./index.css";
 
 export default class CommentList extends Component {
@@ -19,6 +20,8 @@ export default class CommentList extends Component {
             <ul className="list-group">
                 <li className="list-group-item">
                     <div className="handle">
+
+             {   /* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                         <a href="javascript:;">删除</a>
                     </div>
                     <p className="user"><span>xxx</span><span>说:</span></p>
